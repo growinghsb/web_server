cc18b8e0b08c25c19d78167d3d6822cfbe09f23b
growinghsb <seungbeombm@gmail.com>
2021-03-22 15:27:42 +0900

    web study start

---
+++ src/main/java/webserver/WebServer.java
@@ -0,0 +1,35 @@
+package webserver;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server started {} port.", port);
+
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start();
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
