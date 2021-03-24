e05fbe274e244eff2e4297b16ad1e0ee9ed19b34
growinghsb <seungbeombm@gmail.com>
2021-03-24 13:57:46 +0900

    Add 요구사항 3 구현 완료, post로 회원가입

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -4,17 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
 
-    private Utils(){}
+    private Utils() {
+    }
 
-    public static String getHtmlFileName(BufferedReader requestUrl) throws IOException {
-        return requestUrl.readLine().split(" ")[1];
+    public static String[] getRequestHttpHeaderMessages(BufferedReader requestUrl) throws IOException {
+        return requestUrl.readLine().split(" ");
     }
 
     public static byte[] getHtmlFilePath(String resource) throws IOException {
-        return Files.readAllBytes(new File("./src/main/resources/static/" + validation(resource) + ".html").toPath());
+        return Files.readAllBytes(new File("./src/main/resources/static/" + changeHtmlFileName(validation(resource)) + ".html").toPath());
     }
 
     private static String validation(String resource) {
@@ -22,5 +25,45 @@ public class Utils {
             return resource.replace("?", "");
         }
         return resource;
+    }
+
+    private static String changeHtmlFileName(String resource) {
+        if (resource.equals("/")) {
+            return "index";
+        }
+        return resource;
+    }
+
+    public static String getHttpBodyContent(BufferedReader httpHeader) throws IOException {
+        return getContent( getContentLength(httpHeader), httpHeader);
+
+    }
+
+    private static int getContentLength(BufferedReader httpHeader) throws IOException {
+        String contentLength = "";
+        while (httpHeader.ready()) {
+            contentLength = httpHeader.readLine();
+            if (contentLength.contains("Content-Length:")) {
+                break;
+            }
+        }
+        return Integer.parseInt(contentLength.split(" ")[1]);
+    }
+
+    private static String getContent(int contentLength, BufferedReader httpHeader) throws IOException {
+        List<Character> content = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        while (httpHeader.ready()) {
+            content.add((char) httpHeader.read());
+        }
+
+        content = content.subList(content.size() - contentLength, content.size());
+
+        for (Character character : content) {
+            sb.append(character);
+        }
+
+        return sb.toString();
     }
 }
